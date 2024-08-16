7585086368ffa1a3d4c4317f65f9c0da24231546
arturruzich <arutrruzic@gmail.com>
2024-08-16 11:13:43 +0300

    Added tests

---
+++ lesson_9/func_homework_9.py
@@ -0,0 +1,69 @@
+# task 7
+
+
+#def calculate_total_sea_area(black_sea_area, azov_sea_area):
+
+ #   return black_sea_area + azov_sea_area
+
+#black_sea = 436402
+#azov_sea = 37800
+#total_sea_area = calculate_total_sea_area(black_sea, azov_sea)
+#print(f"Total area of Black and Azov seas: {total_sea_area} km2")
+
+
+
+# Task 5
+#def find_longest_word(words):
+   # if not words:
+  #      return ""
+ #   return max(words, key=len)
+
+
+#words_list = ["QA", "Automation", "Python", "Manual", "Engineer"]
+#longest_word = find_longest_word(words_list)
+#print("The longest word is:", longest_word)
+
+
+# Task 9
+#def calculate_computer_cost(monthly_payment, months):
+
+ #   return monthly_payment * months
+
+#monthly_payment = 1179
+#months = 18
+#computer_cost = calculate_computer_cost(monthly_payment, months)
+#print(f"Total price of PC: {computer_cost} UAH")
+
+
+# Task 10
+#def calculate_order_cost(order_list):
+
+  #  total_cost = sum(quantity * price for _, quantity, price in order_list)
+ #   return total_cost
+
+
+#orders = [
+    #("Large pizza", 4, 274),
+    #("Middle pizza", 2, 218),
+   # ("Juice", 4, 35),
+  #  ("Cake", 1, 350),
+ #   ("Water", 3, 21)
+#]
+#order_cost = calculate_order_cost(orders)
+#print(f"Total price: {order_cost} UAH")
+
+
+# Task 6
+#def find_substring(str1, str2):
+
+ #   return str1.find(str2)
+
+#str1 = "Hello, world!"
+#str2 = "world"
+#print(find_substring(str1, str2)) #
+
+#str1 = "The quick brown fox jumps over the lazy dog"
+#str2 = "cat"
+#print(find_substring(str1, str2))
+
+# Finish
